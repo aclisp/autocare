f6f097b089136627ea91195a4f8a2d428fc7f06c
huanghao <huanghao@youxin.cloud>
2025-04-27 13:03:37 +0800

    Use environment variables

--- lib/directus/constants.ts
+++ lib/directus/constants.ts
@@ -1,1 +1,4 @@
-export const DIRECTUS_URL: string = 'http://localhost:8055';
+export const DIRECTUS_URL: string =
+  process.env.NEXT_PUBLIC_DIRECTUS_URL || 'http://localhost:8055';
+export const SERVICE_ACCOUNT_TOKEN: string =
+  process.env.SERVICE_ACCOUNT_TOKEN || '';
--- lib/directus/server-only.ts
+++ lib/directus/server-only.ts
@@ -1,7 +1,7 @@
 import 'server-only';
 import { createDirectus, rest, staticToken } from '@directus/sdk';
-import { DIRECTUS_URL } from './constants';
+import { DIRECTUS_URL, SERVICE_ACCOUNT_TOKEN } from './constants';
 
 export const directusClient = createDirectus(DIRECTUS_URL)
   .with(rest())
-  .with(staticToken(''));
+  .with(staticToken(SERVICE_ACCOUNT_TOKEN));
